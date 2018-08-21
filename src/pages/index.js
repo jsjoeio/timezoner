bd4de6985fa6f9af15a9956751cc5837a4361a35
Joe Previte <jjprevite@gmail.com>
2018-08-20 20:29:11 -0700

    add function to get url parameter and add basic html to display

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]')
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)')
+  var results = regex.exec(location.search)
+  return results === null
+    ? ''
+    : decodeURIComponent(results[1].replace(/\+/g, ' '))
+}
+//SO post https://stackoverflow.com/questions/46970132/convert-us-central-time-to-different-time-zones-using-moment-js
+const mockTimezone = 'America/New_York'
+
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>Original meeting details:</p>
+    <ul>
+      <li>{getUrlParameter('day')}</li>
+      <li>{getUrlParameter('time')}</li>
+      <li>{getUrlParameter('timezone')}</li>
+    </ul>
+    <p>Meeting details in your timezone:</p>
+    <ul>
+      <li />
+      <li />
+      <li>{mockTimezone}</li>
+    </ul>
   </div>
 )
 
