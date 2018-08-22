76c30cbba2cbea31c699b46e17236ba3fe9aff20
Joe Previte <jjprevite@gmail.com>
2018-08-21 18:02:06 -0700

    add test function to convert timezone

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import moment from 'moment-timezone'
 
 function getUrlParameter(name) {
   name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]')
@@ -11,6 +12,15 @@ function getUrlParameter(name) {
 }
 //SO post https://stackoverflow.com/questions/46970132/convert-us-central-time-to-different-time-zones-using-moment-js
 const mockTimezone = 'America/New_York'
+const dateTime = moment.tz(
+  '2018-08-27 5:45:00 PM',
+  'YYYY-MM-DD h:mm:ss a',
+  'America/Phoenix'
+)
+const converted = dateTime
+  .tz('America/New_York')
+  .format('MMMM Do YYYY, h:mm:ss a')
+console.log(converted)
 
 const IndexPage = () => (
   <div>
@@ -24,9 +34,8 @@ const IndexPage = () => (
     </ul>
     <p>Meeting details in your timezone:</p>
     <ul>
-      <li />
-      <li />
       <li>{mockTimezone}</li>
+      <li>{converted}</li>
     </ul>
   </div>
 )
