a64bb56da35f2ec1e842c72c1ff2d686b585929c
Joe Previte <jjprevite@gmail.com>
2018-12-10 17:51:36 -0800

    Create footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ListContainer } from './Footer.styles'
+
+const Footer = () => (
+  <footer>
+    <ListContainer>
+      <li>Download</li>
+      <li>Made by <a href='https://twitter.com/jsjoeio'>@jsjoeio</a></li>
+    </ListContainer>
+  </footer>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import Link from 'gatsby-link'
 import moment from 'moment-timezone'
 import EventCard from '../components/EventCard'
 import { validateURLParams } from '../utils/functions'
+import Footer from '../components/Footer/Footer'
 
 const IndexPage = () => (
   <div>
     {validateURLParams(['day', 'time', 'timezone']) ? <EventCard /> : <h1>Oops!</h1>}
+    <Footer />
   </div>
 )
 
