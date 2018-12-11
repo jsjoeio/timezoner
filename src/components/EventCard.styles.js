b8b21ee40fb38d047e0e60be6b4d31847924846c
Joe Previte <jjprevite@gmail.com>
2018-12-10 17:36:29 -0800

    Refactor EventCard

--- src/components/EventCard.styles.js
+++ src/components/EventCard.styles.js
@@ -1,6 +1,6 @@
 import styled from 'react-emotion'
 
-export const App = styled('div')`
+export const Card = styled('div')`
   background: #ffffff;
   box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.25);
   border-radius: 24px;
