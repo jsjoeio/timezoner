717097a6744218de10e949536e081a8ee73dbc04
Joe Previte <jjprevite@gmail.com>
2018-12-18 14:45:11 -0700

    Change background color and width on EventCard

--- src/components/EventCard.styles.js
+++ src/components/EventCard.styles.js
@@ -1,17 +1,17 @@
 import styled from 'react-emotion'
 
 export const Card = styled('div')`
-  background: #ffffff;
+  background: linear-gradient(359.93deg, #00A6F3 41%, rgba(127, 229, 255, 0) 237.57%);
   box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.25);
   border-radius: 24px;
-  width: 300px;
+  width: 250px;
   margin: 100px auto;
   padding: 60px 45px;
 
   h3 {
     font-size: 18px;
     text-align: center;
-    color: #000000;
+    color: #FFF;
   }
 
   h2 {
@@ -20,7 +20,7 @@ export const Card = styled('div')`
     line-height: normal;
     font-size: 24px;
     text-align: center;
-    color: #000000;
+    color: #FFF;
     margin: 30px auto;
   }
 
@@ -30,7 +30,7 @@ export const Card = styled('div')`
     line-height: normal;
     font-size: 12px;
     text-align: center;
-    color: #000000;
+    color: #FFF;
     margin-bottom: 5px;
   }
 `
