02e0291296ab97709a47d664b6ca1d581b2c4e98
Joe Previte <jjprevite@gmail.com>
2018-12-27 15:45:59 -0700

    Fix window undefined in getUrlParameter function

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,11 +1,13 @@
 export function getUrlParameter(name) {
-  // eslint-disable-next-line
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]')
-  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)')
-  var results = regex.exec(window.location.search)
-  return results === null
-    ? ''
-    : decodeURIComponent(results[1].replace(/\+/g, ' '))
+  if (typeof window !== 'undefined') {
+    // eslint-disable-next-line
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]')
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)')
+    var results = regex.exec(window.location.search)
+    return results === null
+      ? ''
+      : decodeURIComponent(results[1].replace(/\+/g, ' '))
+  }
 }
 
 export function validateURLParams(params) {
