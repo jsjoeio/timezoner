696dc4ba9b24bb122e9c061e7af1bffeef1f1bf4
Joe Previte <jjprevite@gmail.com>
2018-12-27 15:32:40 -0700

    Fix styles on Event card

--- src/components/EventCard.styles.js
+++ src/components/EventCard.styles.js
@@ -4,9 +4,9 @@ export const Card = styled('div')`
   background: linear-gradient(359.93deg, #00A6F3 41%, rgba(127, 229, 255, 0) 237.57%);
   box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.25);
   border-radius: 24px;
-  width: 250px;
+  width: 300px;
   margin: 100px auto;
-  padding: 60px 45px;
+  padding: 50px 45px;
 
   h3 {
     font-size: 18px;
